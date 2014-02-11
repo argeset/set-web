03a864cc71965e3cb9fa65de021ece05ab3b01b2
Serdar Büyüktemiz <hserdarb@gmail.com>
2014-02-11 14:21:03 +0200

    RouteTests added

---
+++ sources/set.web.test/Interface/RouteTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using set.web.test.Shared;
+
+namespace set.web.test.Interface
+{
+    [TestFixture]
+    public class RouteTests : BaseInterfaceTest
+    {
+        [Test]
+        public void should_view_home_index()
+        {
+            var url = string.Format("{0}{1}", BASE_URL, ACTION_HOME);
+
+            GoTo(url);
+            AssertUrl(url);
+
+            CloseBrowser();
+        }
+        
+        [Test]
+        public void should_view_home_contact()
+        {
+            var url = string.Format("{0}{1}", BASE_URL, ACTION_CONTACT);
+
+            GoTo(url);
+            AssertUrl(url);
+
+            CloseBrowser();
+        }
+       
+    }
+}
---
+++ sources/set.web.test/Shared/BaseInterfaceTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium.Firefox;
+
+namespace set.web.test.Shared
+{
+    public class BaseInterfaceTest
+    {
+        public const string BASE_URL = "http://localhost:8011";
+
+        public const string ACTION_HOME = "/home/index";
+        public const string ACTION_CONTACT = "/home/contact";
+
+        public const string ACTION_LOGIN = "/user/login";
+        public const string ACTION_LOGOUT = "/user/logout";
+        public const string ACTION_SIGNUP = "/user/new";
+        public const string ACTION_PROFILE = "/user/detail";
+
+
+        public FirefoxDriver Browser;
+
+        [SetUp]
+        public void Setup()
+        {
+            Browser = new FirefoxDriver();
+        }
+
+        public void GoTo(string url)
+        {
+            Browser.Navigate().GoToUrl(url);
+        }
+
+        public void AssertUrl(string url)
+        {
+            Assert.IsNotNull(Browser);
+            Assert.AreEqual(Browser.Url, url);
+        }
+
+        public void CloseBrowser()
+        {
+            Browser.Close();
+        }
+    }
+}
