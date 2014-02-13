9b332602f39c3e40e3bd3081fa5e960a0f6b3232
Cihan Coşkun <cihancoskun@gmail.com>
2014-02-13 10:59:56 +0200

    added new Behaviour tests and TestBuilders

---
+++ sources/set.web.test/Shared/Builders/HomeControllerBuilder.cs
@@ -0,0 +1,27 @@
+﻿using set.web.Controllers;
+using set.web.Data.Services;
+
+namespace set.web.test.Shared.Builders
+{
+    public class HomeControllerBuilder
+    {
+        private IFeedbackService _feedbackService; 
+
+        public HomeControllerBuilder()
+        {
+            _feedbackService = null; 
+        } 
+
+        internal HomeControllerBuilder WithFeedbackService(IFeedbackService feedbackService)
+        {
+            _feedbackService = feedbackService;
+            return this;
+        }
+
+        internal HomeController Build()
+        {
+            return new HomeController(_feedbackService);
+        }
+
+    }
+}
