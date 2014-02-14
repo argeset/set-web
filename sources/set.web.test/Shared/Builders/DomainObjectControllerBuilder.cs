8937b75b33a150852344ade39b7f8eb820c1704d
Berat Bilgin <bbilginn@gmail.com>
2014-02-14 15:59:33 +0200

    DomainObject test.

---
+++ sources/set.web.test/Shared/Builders/DomainObjectControllerBuilder.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+
+using set.web.Controllers;
+using set.web.Data.Services;
+
+namespace set.web.test.Shared.Builders
+{
+    public class DomainObjectControllerBuilder : BaseBuilder
+    {
+        private IDomainObjectService _domainObjectService;
+
+        public DomainObjectControllerBuilder()
+        {
+            _domainObjectService = new Mock<IDomainObjectService>().Object;
+        }
+
+        internal DomainObjectControllerBuilder WithSearchService(IDomainObjectService feedbackService)
+        {
+            _domainObjectService = feedbackService;
+            return this;
+        }
+
+        internal DomainObjectController Build()
+        {
+            return new DomainObjectController(_domainObjectService);
+        }
+    }
+}
