5ff90248882e4792225f68de2b8ed242ff8d7760
Serdar Büyüktemiz <hserdarb@gmail.com>
2014-02-14 15:33:30 +0200

    form tests added

---
+++ sources/set.web.test/Interface/FormTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+
+using set.web.test.Shared;
+
+namespace set.web.test.Interface
+{
+    [TestFixture]
+    public class FormTests : BaseInterfaceTest
+    {
+        [Test]
+        public void should_save_new_feedback_via_popup_form()
+        {
+            var homeUrl = string.Format("{0}{1}", BASE_URL, ACTION_HOME);
+
+            GoTo(homeUrl);
+
+            Browser.FindElementById("btnOpenFeedBack").Click();
+            Browser.FindElementById("FeedbackMessage").SendKeys("test feedback");
+            Browser.FindElementById("btnSaveFeedback").Click();
+
+            Browser.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(5));
+
+            Assert.IsFalse(Browser.FindElementById("modalFeedback").Displayed);
+
+            CloseBrowser();
+        }
+    }
+}
